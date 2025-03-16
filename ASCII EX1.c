1e6c804c144a17cac02435f766656ee79e0852a7
mrunalchougule <161628567+mrunalchougule@users.noreply.github.com>
2025-03-16 18:21:24 +0530

    Add files via upload

---
+++ ASCII EX1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+	char character;
+	printf("\n Enter any character:");
+	scanf("%c",&character);
+	
+	printf("\n ASCII code=%d",character);
+}
+
+
+//a to z   :97 to 122
+//A to Z   :65 to 90
+//0 to 9   :48 to 57
+//special symbol :-128 to 47, 58 to 64, 91 to 96, 123 to 127
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ASCII EX2,c.cpp
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+int main()
+{
+	int digit;
+	
+	
+	printf("\n Enter any digit:");
+	scanf("%d",&digit);
+	
+	printf("\n ASCII character=%c",digit);
+}
