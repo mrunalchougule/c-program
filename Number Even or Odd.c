f309aec182d1fbab9a09bcdbeb0c33213cd0ff0d
mrunalchougule <161628567+mrunalchougule@users.noreply.github.com>
2025-04-05 21:18:16 +0530

    Add files via upload

---
+++ Number Even or Odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int num; 
+	
+	printf("\n Enter the Number:");
+	scanf("%d",&num);
+	 
+	if(num%2==0)
+	{
+		printf("\n Number is Even:");
+	}
+	else
+	{
+		printf("\n Number is Odd:");
+	}
+}
