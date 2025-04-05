1f13908f2a7c7320f672e5e8aa3b9c5356018d8b
mrunalchougule <161628567+mrunalchougule@users.noreply.github.com>
2025-04-05 21:19:24 +0530

    Add files via upload

---
+++ Switch case ex1.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+void main()
+{
+	int digit;
+	
+	printf("\n Enter digit:");
+	scanf("%d",&digit);
+	
+	switch(digit)
+	{
+	    case 0:
+		       printf("\n it is zero:");
+	           break;
+		
+    	case 1:
+	          printf("\n it is one:");
+		      break;
+			
+	    case 2:
+		      printf("\n it is two:");
+		      break;
+		
+	    case 4:
+		      printf("\n it is four:");
+		      break;
+		
+	    case 5:
+		      printf("\n it is five:");
+		      break;
+		
+	    case 6:
+		      printf("\n it is six:");
+		      break;
+		
+	    case 7:
+		     printf("\n it is seven:");
+		     break;
+		
+	    case 8:
+		      printf("\n it is eight:");
+		      break;
+		
+	    case 9:
+		      printf("\n it is night:");
+		      break;
+		
+	    case 10:
+		       printf("\n it is ten:");
+		       break;
+		
+	    default:
+		       printf("\n it is invalid digit:");	
+    }
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
