45a105e8db4908fe3dde8881adf5e1902f3082bd
mrunalchougule <161628567+mrunalchougule@users.noreply.github.com>
2025-04-10 22:01:52 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	char ch;
+	
+	printf("\n Enter any charcter:");
+	scanf("%c",&ch);
+	
+	if(ch=='a')
+	{
+		printf("\n It is vowel:");
+	}
+	else if(ch=='e')
+	{
+		printf("\n It is vowel:");
+	}
+	else
+	{
+		printf("\n It is consonant:");
+	}
+}
