97b43cd38ecfe6d59acdda6047afdb3d170e2cfe
mrunalchougule <161628567+mrunalchougule@users.noreply.github.com>
2025-04-30 23:00:37 +0530

    Add files via upload

---
+++ AssignmentQ6.c
@@ -0,0 +1,35 @@
+//Area of rectangle&perimeter
+
+#include<stdio.h>
+void main()
+{
+	int width,height,perimeter,area;
+	printf("\n Enter width:");
+	scanf("%d",&width);
+	
+	printf("\n Enter height :");
+	scanf("%d",&height);
+	
+	area=width*height;
+	printf("\n Enter area of rectangle=%d",area);
+	
+	area=2*width+2*height;
+	printf("\n Enter perimeter of rectangle=%d",area);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
